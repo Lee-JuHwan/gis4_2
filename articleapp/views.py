da5022f57d6f4a7c48090d5f72d70977ce0ecac7
Lee-JuHwan <ljk8190@naver.com>
2021-08-04 17:26:37 +0900

    8/4 commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
-from articleapp.forms import ArticleCreationFrom
+from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
 
 class ArticleCreateView(CreateView):
     model = Article
-    form_class = ArticleCreationFrom
+    form_class = ArticleCreationForm
     template_name = 'articleapp/create.html'
 
     def form_valid(self, form):
@@ -24,3 +24,12 @@ class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
