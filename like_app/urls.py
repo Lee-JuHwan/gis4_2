51607cf9f8aa11de43293aa4880fe8e14eb526ed
Lee-JuHwan <ljk8190@naver.com>
2021-08-24 12:42:23 +0900

    8/24 commit 3 - like_app (view & detailtemplates)

--- like_app/urls.py
+++ like_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from like_app.views import LikeArticleView
+
 app_name = 'like_app'
 
 urlpatterns = [
-
+    path('article/<int:article_pk>', LikeArticleView.as_view(), name='article_like')
 ]
