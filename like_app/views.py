32d3cad175af3b1fa4e35cf595f8563c31f2fd8d
Lee-JuHwan <ljk8190@naver.com>
2021-08-24 12:50:48 +0900

    8/24 commit 4 - like_app (like message)

--- like_app/views.py
+++ like_app/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -18,14 +19,17 @@ class LikeArticleView(RedirectView):
         like_record = LikeRecord.objects.filter(user=user, article=article)
 
         if like_record.exists():
+            messages.add_message(request, messages.ERROR, '이미 좋아요를 눌렀습니다')
             return HttpResponseRedirect(reverse('articleapp:detail',
                                                 kwargs={'pk': kwargs['article_pk']}))
         else:
             LikeRecord(user=user, article=article).save()
 
-            article.like += 1
-            article.save()
-            return super().get(request, *args, **kwargs)
+        article.like += 1
+        article.save()
+
+        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다')
+        return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']})
