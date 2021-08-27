b88915a2c47bbc523529c6a200611562662bef75
Lee-JuHwan <ljk8190@naver.com>
2021-08-27 11:26:46 +0900

    8/27 commit 1 - delete hello_world

--- accountapp/views.py
+++ accountapp/views.py
@@ -15,33 +15,33 @@ from accountapp.forms import AccountCreationForm
 from accountapp.models import NewModel
 from articleapp.models import Article
 
-
-@login_required(login_url=reverse_lazy('accountapp:login'))
-def hello_world(request):
-    if request.method == "POST":
-
-        temp = request.POST.get('next')
-        request.Get.get('next')
-
-        new_model = NewModel()
-        new_model.text = temp
-        new_model.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        # data_list = NewModel.objects.all()
-        # return render(request, 'accountapp/hello_world.html',
-        #               context={'data_list': data_list})
-    else:
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'data_list': data_list})
+# 필요없어져 삭제
+# @login_required(login_url=reverse_lazy('accountapp:login'))
+# def hello_world(request):
+#     if request.method == "POST":
+#
+#         temp = request.POST.get('next')
+#         request.Get.get('next')
+#
+#         new_model = NewModel()
+#         new_model.text = temp
+#         new_model.save()
+#
+#         return HttpResponseRedirect(reverse('accountapp:hello_world'))
+#         # data_list = NewModel.objects.all()
+#         # return render(request, 'accountapp/hello_world.html',
+#         #               context={'data_list': data_list})
+#     else:
+#         data_list = NewModel.objects.all()
+#         return render(request, 'accountapp/hello_world.html',
+#                       context={'data_list': data_list})
 
 
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name ='accountapp/create.html'
 
 
@@ -76,5 +76,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
